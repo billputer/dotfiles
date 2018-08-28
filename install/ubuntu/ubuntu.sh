522b229bbd8725eaa26258aa31a4869ee9ebd249
Bill Wiens <billwiens@gmail.com>
2018-08-28 09:52:16 -0700

    Add prompt to install some ubuntu utilities

--- install/ubuntu/ubuntu.sh
+++ install/ubuntu/ubuntu.sh
@@ -6,5 +6,27 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+read -p 'Do you want to `apt-get install` utilities ? ' -n 1 -r choice
+echo # move to a new line
+if [[ $choice =~ ^[Yy]$ ]]; then
 
-echo "nothing to set up for ubuntu yet"
+  # install various utilities
+  sudo apt-get install -y \
+    ack-grep \
+    curl \
+    htop \
+    iftop \
+    iotop \
+    iperf3 \
+    jq \
+    ncdu \
+    nmap \
+    rename \
+    tcpdump \
+    tmux \
+    tree \
+    vim \
+    zsh \
+    ;
+
+fi
