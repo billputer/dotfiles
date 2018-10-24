1d8bb70f0e815fb2a418481d4538cfa0f8af9e4a
Bill Wiens <bill.wiens@liquidplanner.com>
2018-10-24 14:38:11 -0700

    Revamp hyper configuration

--- hyper.js
+++ hyper.js
@@ -1,25 +1,21 @@
 module.exports = {
   config: {
     // default font size in pixels for all tabs
-    fontSize: 16,
+    fontSize: 17,
 
     // font family with optional fallbacks
     fontFamily: '"Inconsolata-g for Powerline", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
-
-    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: '#839496',
 
     // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
     cursorShape: 'BLOCK',
 
     // color of the text
     foregroundColor: '#839496',
-
-    // terminal background color
+    /// terminal background color
     backgroundColor: '#002833',
-
-    // border color (window, tabs)
-    borderColor: '#586e75',
+    
+    // text selection
+    selectionColor: "rgba(170,120,0,0.6)",
 
     // custom css to embed in the main window
     css: '',
@@ -31,37 +27,16 @@ module.exports = {
     // set to `true` if you're using a Linux set up
     // that doesn't shows native menus
     // default: `false` on Linux, `true` on Windows (ignored on macOS)
-    showHamburgerMenu: '',
+    showHamburgerMenu: 'false',
 
     // set to `false` if you want to hide the minimize, maximize and close buttons
     // additionally, set to `'left'` if you want them on the left, like in Ubuntu
     // default: `true` on windows and Linux (ignored on macOS)
-    showWindowControls: '',
+    // showWindowControls: 'left',
 
     // custom padding (css format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
+    padding: '4px 4px',
 
-    // the full list. if you're going to provide the full color palette,
-    // including the 6 x 6 color cubes and the grayscale map, just provide
-    // an array here instead of a color map object
-    colors: {
-      black: '#002b36',
-      red: '#dc322f',
-      green: '#859900',
-      yellow: '#b58900',
-      blue: '#268bd2',
-      magenta: '#d33682',
-      cyan: '#2aa198',
-      white: '#eee8d5',
-      lightBlack: '#002833',
-      lightRed: '#cb4b16',
-      lightGreen: '#586e75',
-      lightYellow: '#657b83',
-      lightBlue: '#839496',
-      lightMagenta: '#6c6ec6',
-      lightCyan: '#93a1a1',
-      lightWhite: '#fdf6e3',
-    },
 
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
     // if left empty, your system's login shell will be used by default
@@ -82,6 +57,7 @@ module.exports = {
 
     // URL to custom bell
     // bellSoundURL: 'http://example.com/bell.mp3',
+    scrollback: 10000,
 
     // for advanced config flags please refer to https://hyper.is/#cfg
   },
@@ -92,10 +68,14 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyperterm-mactabs'],
+  // plugins: ['hyperterm-mactabs'],
+  plugins: ["hyper-solarized-dark", "hyper-search"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
   localPlugins: [],
+  keymaps: {
+    'window:devtools': 'cmd+alt+o'
+  }
 };
