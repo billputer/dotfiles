9157d3ebf3c5f58c886674333609855364cf9be7
Bill Wiens <bill.wiens@liquidplanner.com>
2019-07-29 15:46:56 -0700

    Remove old apps from brew-cask install

--- install/osx/brew-cask.sh
+++ install/osx/brew-cask.sh
@@ -14,8 +14,6 @@ brew cask install atom
 brew cask install bartender
 brew cask install caffeine
 brew cask install docker
-brew cask install electric-sheep
-brew cask install flux
 brew cask install google-chrome
 brew cask install gpgtools
 brew cask install hammerspoon
@@ -23,7 +21,6 @@ brew cask install istat-menus
 brew cask install iterm2
 brew cask install karabiner-elements
 brew cask install omnidisksweeper
-brew cask install pgadmin4
 brew cask install scroll-reverser
 brew cask install slack
 brew cask install the-unarchiver
