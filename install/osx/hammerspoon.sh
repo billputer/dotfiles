158d3cf026959b248d351fba566c2368381e91f8
Bill Wiens <billwiens@gmail.com>
2022-02-11 14:08:12 -0800

    Update Hammerspoon to manually build hs._asm.undocumented.spaces

--- install/osx/hammerspoon.sh
+++ install/osx/hammerspoon.sh
@@ -10,17 +10,20 @@ if [ -d "/Applications/Hammerspoon.app" ]; then
   echo "hammerspoon already installed"
 else
   echo "installing hammerspoon"
-  brew cask install Caskroom/cask/hammerspoon
+  brew install hammerspoon
 fi
 
 if [ -d "$HOME/.files/hammerspoon/hs/_asm/undocumented/spaces" ]; then
   echo "hammerspoon hs._asm.undocumented.spaces module already installed"
 else
   echo "installing hammerspoon hs._asm.undocumented.spaces module"
-  pushd ~/.files/hammerspoon
-  curl -OL https://github.com/asmagill/hs._asm.undocumented.spaces/releases/download/v0.5/spaces-v0.5.tar.gz
-  tar -xvzf spaces-v0.5.tar.gz
-  rm spaces-v0.5.tar.gz
+  mkdir -p "$HOME/src"
+  git clone git@github.com:asmagill/hs._asm.undocumented.spaces.git
+
+  pushd "$HOME/src/hs._asm.undocumented.spaces"
+  HS_APPLICATION=/Applications PREFIX=~/.hammerspoon make install-arm64
   popd
+
+  echo "Don't forget to allow Hammerspoon in System Preferences > Security & Privacy > Privacy > Accessibility"
 fi
 exit 0
